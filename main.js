cf6952cb9ec098f3a2ecf322d7770ee1cd2b8467
david30-maker <95672514+david30-maker@users.noreply.github.com>
2023-02-14 10:50:45 +0100

    Add html, css, javascript file

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+<div class="guest">
+<div class="image_design">
+  <img src="${guest.images.imagesUrl}" alt="${guest.images.imagesAlt}">
+</div>
+<aside>
+  <div>
+    <h4>${guest.name}</h4>
+    <p>${guest.shortDescription}</p>
+    <hr width="30">
+  </div>
+  <span>${guest.profile}</span>
+</aside>
+</div>
